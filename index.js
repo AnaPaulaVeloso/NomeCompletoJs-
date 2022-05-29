3ff960e321c672ff518aa5ded7a0fec92ef1c265
AnaPaulaVeloso <velosoanapaula29@gmail.com>
2022-05-29 13:24:41 -0300

    Realizar Nome Completo em Js

---
+++ index.js
@@ -0,0 +1,6 @@
+let nome = prompt('Qual é seu nome ?');
+
+let sobrenome = prompt ('Digite seu sobrenome');
+
+alert(`Nome completo; ${nome.toUpperCase()} ${sobrenome}`);
+alert(`Nome catologo; ${sobrenome.toUpperCase()} ${nome}`);
